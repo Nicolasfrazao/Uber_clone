254ef78de5f13ae3fa41e54c96bca17204876602
Nicolas Frazao <nicolas_frazao@hotmail.com>
2024-08-27 09:02:33 -0300

    vercel deployed and stripe added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,48 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import {ClerkProvider, ClerkLoaded} from '@clerk/clerk-expo';
+import * as SecureStore from 'expo-secure-store';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const publicKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`key: ${key} was used \n`);
+      } else {
+        console.log(`No value stored under key: ${key}`);
+      }
+      return item;
+    } catch (error) {
+      console.error(`Secure store error: ${error}`);
+      return null;
+    }
+  },
+
+  async saveToken(key: string, value: string) {
+    try {
+      await SecureStore.setItemAsync(key, value);
+      console.log(`key: ${key} was saved`);
+    } catch (error) {
+      console.error(`Secure store error: ${error}`);
+      return null;
+    }
+  },
+};
+
+if(!publicKey) throw new Error('Missing Clerk Publishable Key')
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -27,11 +61,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <ClerkProvider publishableKey={publicKey}>
+      <ClerkLoaded>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Slot />
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </ThemeProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
